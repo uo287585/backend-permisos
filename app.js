fd7af7b42c3bff79cead31053835a18025f5df2a
uo287585 <uo287585@uniovi.es>
2024-05-07 19:53:13 +0200

    post method sending json

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ let express = require("express")
 let app = express()
 let port = 8081
 
+//leer datos del body en formato json
+app.use(express.json())
 
 let routerPermissions= require("./routers/routerPermissions")
 app.use("/permissions", routerPermissions)
 
-app.listen(por, ()=>{
+app.listen(port, ()=>{
     console.log("Servidor activo en "+port)
 })
--- routers/routerPermissions.js
+++ routers/routerPermissions.js
@@ -6,4 +6,19 @@ routerPermissions.get("/", (req,res)=>{
     res.json(permissions)
 })
 
+routerPermissions.post("/",(req,res)=>{
+    let text = req.body.text
+    let userId = req.body.userId
+    let lastId = permissions[permissions.length-1].id
+
+    permissions.push({
+        id:lastId+1,
+        text:text,
+        approbedBy:[],
+        userId:userId
+    })
+
+    res.json({id: lastId+1})
+})
+
 module.exports = routerPermissions
