5af740e16112c1e3ddd201a04d846365a07b6eff
uo287585 <uo287585@uniovi.es>
2024-05-08 14:14:18 +0200

    post users

--- routers/routerUsers.js
+++ routers/routerUsers.js
@@ -18,5 +18,33 @@ routerUsers.get("/:id", (req,res)=>{
     res.json({id: user.id, email:user.email})
 })
 
+routerUsers.post("/",(req,res)=>{
+    let email = req.body.email
+    let password = req.body.password
+
+    let errores = []
+
+    if(email==undefined)
+        errores.push("no email")
+    if(users.find(u=>u.email==email)!=undefined)
+        errores.push("user already exists")
+    if(password==undefined)
+        errores.push("no password")
+    if(password!=undefined && password.length<4)
+        errores.push("password less than 4")
+
+    if(errores.length>0){
+        return res.status(400).json({errores:errores})
+    }
+
+    users.push({
+        id: users.length+1,
+        email:email,
+        password:password
+    })
+
+    res.json({lastId: users.length})
+})
+
 
 module.exports = routerUsers
